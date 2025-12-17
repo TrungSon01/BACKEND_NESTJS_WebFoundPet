6a16a99bb6da68e4c788ac488b5f3bb9ffb6802f
Nguyễn Trung Sơn <traidatmaxmanh@gmail.com>
2025-12-17 15:01:27 +0700

    deploy

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { app_constant } from './common/constant/app.constant';
 import { Logger } from '@nestjs/common';
-async function bootstrap() {
-  const logger = new Logger();
 
-  const PORT = app_constant.PORT;
+async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
+
+  // Global prefix
   app.setGlobalPrefix('api');
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: (origin, callback) => {
+      callback(null, true);
+    },
+    credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
   });
-  await app.listen(PORT ?? 3000, () => {
-    logger.verbose(`Server is running in http://localhost:${PORT}/api`);
-  });
+
+  const port = process.env.PORT || 8001;
+
+  await app.listen(port, '0.0.0.0');
+
+  logger.log(`🚀 Server running on port ${port}`);
 }
+
 bootstrap();
